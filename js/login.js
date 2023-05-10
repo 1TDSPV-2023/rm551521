98ae38e534acab45be22db29cbfbf28c5c4ca611
patrickjaguski <patrick_jaguski@hotmail.com>
2023-05-10 20:22:37 -0300

    ajustando meu login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 "use strict";
 
 //CRIANDO OBJETOS
-
 const usuario1 = {
     nomeUsuario : "rita-lee",
     senhaUsuario : "00000"
@@ -12,37 +11,35 @@ const usuario2 = {
     senhaUsuario : "12345"
 }
 
-
 //LISTA DE USUÁRIOS
 let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1)
-listaDeUsuarios.push(usuario2)
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
 
+addEventListener("click", (evento)=>{
+     
+    let userInput = document.querySelector("#idUser"); 
+    let passInput = document.querySelector("#idPass"); 
 
-
-addEventListener("click", (evento)=> {
-  
-    let userInput = document.querySelector("#idUser");
-    let passInput = document.querySelector("#idPass");
-
-
-
-    if(evento.target.id == "btnSubmit"){
-
-        listaDeUsuarios.forEach( (usuario)=> {
-            
-        } );
-
+        if(evento.target.id == "btnSubmit"){
         
-        
-        if(userInput.value == "rm551521" && passInput.value == "12345"){
-            console.log("USUÁRIO VALIDADO!");
+        try{
+                listaDeUsuarios.forEach((usuario)=>{
 
-        } else{
+                    if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
+                        throw "USUÁRIO VALIDADO!";
+                    }
+                });
+
+                throw "USUÁRIO OU SENHA INCORRETOS!";
+
+    }catch(msg){
+        if(msg == "USUÁRIO VALIDADO!"){
+            console.log("USUÁRIO VALIDADO!")
+        }else{
             console.log("USUÁRIO OU SENHA INCORRETOS!");
         }
-
-        
-
     }
+
+  }
 });
