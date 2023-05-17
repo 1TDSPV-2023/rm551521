8882af9172ce13eab12e81a39e14cc358daffc51
patrickjaguski <patrick_jaguski@hotmail.com>
2023-05-16 23:00:14 -0300

    validando formulario

--- js/login.js
+++ js/login.js
@@ -1,30 +1,66 @@
 "use strict";
 
-//CRIANDO OBJETOS
-const usuario1 = {
-    nomeUsuario : "rita-lee",
-    senhaUsuario : "00000"
-}
-
-const usuario2 = {
-    nomeUsuario : "ptt01",
-    senhaUsuario : "12345"
-}
-
 //LISTA DE USUÁRIOS
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1);
-listaDeUsuarios.push(usuario2);
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "João Paulino",
+        nomeUsuario : "joao",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Maria Tomaite",
+        nomeUsuario : "maria",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Paulo da Selva",
+        nomeUsuario : "paulo",
+        senhaUsuario : "123456"
+    }
+    ,
+];
+
+localStorage.setItem("listUser",JSON.stringify (listaDeUsuarios));
+
+
+
 
 addEventListener("click", (evento)=>{
      
     let userInput = document.querySelector("#idUser"); 
     let passInput = document.querySelector("#idPass"); 
 
+    // //MOSTRAR SENHA
+    // const eyePass = document.querySelector(".fa-eye");
+
+    if(evento.target.className =="fa fa-eye" || evento.target.className == "fa fa-eye-slash"){
+        evento.target.setAttribute("style", "cursor:pointer")
+        if(passInput.getAttribute("type") == "password"){
+            passInput.setAttribute("type", "text");
+            evento.target.setAttribute("class", "fa fa-eye-slash")
+        }else{
+            evento.target.setAttribute("style", "cursor:pointer");
+            evento.target.setAttribute("class", "fa fa-eye");
+            passInput.setAttribute("type","password");
+        }
+}
+
+
+    let listaDeUsuariosRecuperada = JSON.parse(localStorage.getItem("ListaUser"));
+
+
         if(evento.target.id == "btnSubmit"){
         
         try{
-                listaDeUsuarios.forEach((usuario)=>{
+                listaDeUsuariosRecuperada.forEach((usuario)=>{
 
                     if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
                         throw "USUÁRIO VALIDADO!";
@@ -35,7 +71,8 @@ addEventListener("click", (evento)=>{
 
     }catch(msg){
         if(msg == "USUÁRIO VALIDADO!"){
-            console.log("USUÁRIO VALIDADO!")
+            //redirect
+            window.location.href = "../sucesso.html";
         }else{
             console.log("USUÁRIO OU SENHA INCORRETOS!");
         }
